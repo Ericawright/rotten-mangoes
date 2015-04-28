4cdb3a095a4d14e47b0a0f063e92291d31d6b89c
Erica Wright <ericaewright@gmail.com>
2015-04-28 21:22:02 +0000

    upload movie poster image

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,10 +3,11 @@ class Movie < ActiveRecord::Base
   validates :director, presence: true
   validates :runtime_in_minutes, numericality: { only_integer: true }
   validates :description, presence: true
-  validates :poster_image_url, presence: true
+  validates :image, presence: true
   validates :release_date, presence: true
   validate :release_date_is_in_the_future
   has_many :reviews
+  mount_uploader :image, ImageUploader
 
 
   def review_average
