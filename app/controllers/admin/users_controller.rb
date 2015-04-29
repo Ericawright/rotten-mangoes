072d9c71478af406bbb1c40009f6d9b8446c41b6
Erica Wright <ericaewright@gmail.com>
2015-04-29 01:52:51 +0000

    admin can delete users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -4,12 +4,6 @@ class Admin::UsersController < ApplicationController
   def index
     @users = User.all
   end
-
-  def destroy
-    @user = User.find(params[:id])
-    @user.destroy
-    redirect_to admin_users_path
-  end
   
   def edit
     @user = User.find(params[:id])
@@ -19,15 +13,15 @@ class Admin::UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  def update
-    @user = User.find(params[:id])
+  # def update
+  #   @user = User.find(params[:id])
 
-    if @user.update_attributes(user_params)
-      redirect_to admin_user_path(@user)
-    else
-      render :edit
-    end
-  end
+  #   if @user.update_attributes(user_params)
+  #     redirect_to admin_user_path(@user)
+  #   else
+  #     render :edit
+  #   end
+  # end
 
   # def new
   #   @user = User.new
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_users_path
+  end
+
 
   protected
 
